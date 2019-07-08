28cae0ec61e42278ce539edff5681f016b65a134
Darin Armstrong <darin.marmstrong@gmail.com>
2019-07-08 09:52:33 -0600

    Initial Commit

---
+++ BulletinBoard/Controllers/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BulletinBoard
+//
+//  Created by Darin Marcus Armstrong on 7/8/19.
+//  Copyright © 2019 Darin Marcus Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
